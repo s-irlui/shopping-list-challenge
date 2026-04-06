0a85ab7c8eb1df3b49936429356b8134c38cba03
Louis Gitonga <louisgitonga19@gmail.com>
2026-04-06 22:29:04 +0300

    first commit

---
+++ map.js
@@ -0,0 +1,19 @@
+const studentMarks =[83, 43, 58, 90, 100];
+
+const marksRevision = studentMarks.map(studentMark => studentMark + 5);
+console.log(marksRevision);
+
+// filter
+
+const belowaverageMarks = studentMarks.filter(studentMark => studentMark < 60);
+console.log(belowaverageMarks);
+
+//reduce
+
+const totalMarks = studentMarks.reduce((total, studentMark) => total + studentMark, 0);
+console.log(totalMarks);   
+
+//find
+
+const findnumber = marksRevision.find(mark => mark === 105);
+console.log(findnumber);
