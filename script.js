d3cfc7b1c70b88801fb6fd46f1dd3c2e8373f2fb
Louis Gitonga <louisgitonga19@gmail.com>
2026-04-06 22:33:59 +0300

    html and js

---
+++ script.js
@@ -0,0 +1,8 @@
+// Array to store items
+let shoppingList = [];
+
+// Get elements
+const itemInput = document.getElementById("itemInput");
+const itemPrice = document.getElementById("itemPrice");
+const addBtn = document.getElementById("addBtn");
+const shoppingListEl = document.getElementById("shoppingList");
